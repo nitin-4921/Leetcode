1190c902f1a6e0e9426b4dc47a34f637b8518893
Nitin kumar <nitin1406b@gmail.com>
2025-11-02 21:24:09 +0530

    Time: 4 ms (60.13%), Space: 50.3 MB (9.14%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // top-left corner (start)
+                if (i == 0 && j == 0) continue;
+
+                // first row — can only come from left
+                else if (i == 0)
+                    grid[i][j] += grid[i][j - 1];
+
+                // first column — can only come from top
+                else if (j == 0)
+                    grid[i][j] += grid[i - 1][j];
+
+                // else, take min from top or left
+                else
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+}
