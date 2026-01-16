4d05f65ff74c579951ac9fa5b10f534287e71f12
Nitin kumar <nitin1406b@gmail.com>
2026-01-16 20:47:43 +0530

    Time: 1 ms (97.67%), Space: 42.6 MB (69.76%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+        if (num < 10){
+            return num;
+        }
+
+        int sum = 0 ;
+        while (num > 0){
+            int rem = num % 10;
+            sum += rem;
+            num /= 10;
+        }
+
+        return addDigits(sum);
+        
+    }
+}
