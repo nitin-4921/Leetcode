c41f6282870badd2849574381396ca4a7c765e9b
Nitin kumar <nitin1406b@gmail.com>
2026-03-30 11:44:07 +0530

    Time: 0 ms (100%), Space: 41.8 MB (95.94%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        
+        int c = 0 ;
+        int xor = x ^ y ;
+        int mask = 1 ;
+        for(int i = 0 ; i < 32 ; i++){
+            if((xor & mask) != 0){
+                c++;
+            }
+            mask <<= 1 ;
+        }
+
+        return c ;
+
+        
+    }
+}
