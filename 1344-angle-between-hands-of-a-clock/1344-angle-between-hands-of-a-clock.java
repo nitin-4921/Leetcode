5f10df6996342603a5a37a1d16c1391f01e72603
Nitin kumar <nitin1406b@gmail.com>
2026-06-18 21:30:05 +0530

    Time: 0 ms (100%), Space: 44.7 MB (87.91%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double min = minutes * 6.0;
+        double hr = (hour % 12) * 30.0 + minutes * 0.5;
+
+        double diff = Math.abs(hr - min);
+        return Math.min(diff, 360.0 - diff);
+    }
+}
