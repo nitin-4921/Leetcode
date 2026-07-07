1a149f84f852d903ca6fe3f500624a21a23331b6
Nitin kumar <nitin1406b@gmail.com>
2026-07-07 21:46:59 +0530

    Time: 0 ms (100%), Space: 42.4 MB (15.69%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int set(int n, int[] dp) {
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = set(n - 1, dp) + set(n - 2, dp) + set(n - 3, dp);
+
+        return dp[n];
+    }
+
+    public int tribonacci(int n) {
+
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        return set(n, dp);
+    }
+}
